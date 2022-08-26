4104610d24ff4f6495edba36eee5b1f6ba211114
cooh8 <l>
2022-08-26 10:27:49 +0800

    初步使用node运行服务

---
+++ Day1-初识Node/01-test.js
@@ -0,0 +1,12 @@
+//创建一个node
+(function(){
+    let http = require('http')
+    http.createServer(function(request,response){
+        //设置响应信息
+        response.writeHead(200,{'Content-Type':'text/plain'})
+        response.end('Hello World\n')
+    }).listen(8888)
+
+    //打印终端
+    console.log('Server running port is 8888')
+})();
